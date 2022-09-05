5705bd6ba02082a1e1d2c4b3951075c263929ea5
Deliciaes <71063351+Deliciaes@users.noreply.github.com>
2022-09-05 17:46:22 +0200

    edit index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import styles from "../styles/Home.module.css";
+import Head from "next/head";
 import client from "../contentful";
 
 export async function getStaticProps() {
@@ -9,8 +9,20 @@ export async function getStaticProps() {
     },
   };
 }
-
 export default function Home({ testmodels }) {
   console.log(testmodels);
-  return <div className={styles.container}>hej</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Majornas Böcker & Kaffe</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <p className="mt-3 text-2xl">check the console</p>
+      </main>
+
+      <footer className="flex items-center justify-center w-full h-24 border-t"></footer>
+    </div>
+  );
 }
