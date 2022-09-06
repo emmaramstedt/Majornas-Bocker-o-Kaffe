a94b6f73e8fc24413ac4b817d71829b57c7a9467
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2022-09-06 22:23:03 +0200

    Added Instagram feed to gallery page.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      'scontent-arn2-2.cdninstagram.com',
+      'scontent-arn2-1.cdninstagram.com',
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
