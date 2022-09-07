8eef400bba45e88dc9a88aae793326ca55e95bfa
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2022-09-07 15:02:36 +0200

    Added dynamic routes to images.

--- next.config.js
+++ next.config.js
@@ -3,10 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [
-      'scontent-arn2-2.cdninstagram.com',
-      'scontent-arn2-1.cdninstagram.com',
-    ],
+    loader: 'imgix',
+    path: '',
   },
 };
 
