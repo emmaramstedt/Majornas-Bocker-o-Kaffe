140b9861a076442de6a25ed44c348fbdf2e789f0
Deliciaes <71063351+Deliciaes@users.noreply.github.com>
2022-09-16 14:36:30 +0200

    about-top styled

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,24 +2,24 @@ import ActiveLink from "./ActiveLink.tsx";
 
 function Navbar() {
   return (
-    <nav className="w-full font-nav">
+    <nav className="w-full font-nav mb-[40px]">
       <ul className="flex h-[48px] w-full p-0 m-0">
         <ActiveLink
           activeClassName="active lg:flex-[2.5] lg:transition-all lg:duration-1000 ease-out"
           href="/"
         >
           <li className="flex justify-center items-center overflow-hidden h-full border-solid border border-[#111827] rounded-[64px] cursor-pointer">
-            <a href="" className="flex">
+            <a className="flex">
               OM OSS <div className="navDot"></div>
             </a>
           </li>
         </ActiveLink>
         <ActiveLink
           activeClassName="active lg:flex-[2.5] lg:transition-all lg:duration-1000 ease-out"
-          href="about"
+          href="nyheter"
         >
           <li className="flex justify-center items-center overflow-hidden h-full border-solid border border-[#111827] rounded-[64px] cursor-pointer">
-            <a href="" className="flex">
+            <a className="flex">
               NYHETER <div className="navDot"></div>
             </a>
           </li>
