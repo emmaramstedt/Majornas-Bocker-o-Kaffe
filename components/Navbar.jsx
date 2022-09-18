57ac34098b59b9023be7104099eb90e118e3da37
Deliciaes <71063351+Deliciaes@users.noreply.github.com>
2022-09-18 15:05:13 +0200

    tailwind fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,21 +3,21 @@ function Navbar() {
   return (
     <nav className="w-full font-nav mb-[40px]">
       <ul className="flex basis-auto h-[48px] w-full p-0 m-0">
-        <ActiveLink activeClassName="active grow-[2.5]" href="/">
+        <ActiveLink activeClassName="active" href="/">
           <li className="flex justify-center items-center grow shrink basis-auto transition-all duration-1000 ease-out border-solid border border-[#111827] rounded-[64px] cursor-pointer">
             <a className="flex">
               OM OSS <div className="navDot"></div>
             </a>
           </li>
         </ActiveLink>
-        <ActiveLink activeClassName="active grow-[2.5]" href="nyheter">
+        <ActiveLink activeClassName="active" href="nyheter">
           <li className="flex justify-center items-center grow shrink basis-auto transition-all duration-1000 ease-out border-solid border border-[#111827] rounded-[64px] cursor-pointer">
             <a className="flex">
               NYHETER <div className="navDot"></div>
             </a>
           </li>
         </ActiveLink>
-        <ActiveLink activeClassName="active grow-[2.5]" href="/kontakt">
+        <ActiveLink activeClassName="active" href="/kontakt">
           <li className="flex justify-center items-center grow shrink basis-auto transition-all duration-1000 ease-out border-solid border border-[#111827] rounded-[64px] cursor-pointer">
             <a className="flex">
               KONTAKT <div className="navDot"></div>
