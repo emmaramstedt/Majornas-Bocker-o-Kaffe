ad3c1efd86b2f4e10ff66f251430cedc2987cc99
Deliciaes <71063351+Deliciaes@users.noreply.github.com>
2022-09-27 14:52:05 +0200

    fixed sorting
    now sorting past posts separately so that they end up neatly behind all the other ones, instead of messing around with flex order which can make the DOM a bit messy.

--- components/events/EventsCard.jsx
+++ components/events/EventsCard.jsx
@@ -8,22 +8,21 @@ const EventsCard = ({
   EventTime,
   EventLink,
   EventLinkText,
-  Myref,
 }) => {
   return (
     <>
       <div
-        className={`card isHidden xs:max-w-[312px] md:w-[312px] h-[270px] border-solid border border-[#111827] rounded-t-lg mb-[32px] ${
+        className={`card isHidden xs:max-w-[312px] md:w-[312px] h-[300px] border-solid border border-[#111827] rounded-t-lg mb-[32px] ${
           EventDate >= moment().format("YYYY-MM-DD") ? "" : "bg-[#f2f2f2]"
         }`}
-        id={Myref}
       >
         <div
-          className={
+          className={`h-full ${
             EventDate >= moment().format("YYYY-MM-DD")
               ? EventCategory
               : "datePast"
           }
+        `}
         >
           <div className="h-[63px] flex">
             <div className="w-[100%] eventDecoOne rounded-tl-lg flex justify-center items-center font-ibmBold">
@@ -32,29 +31,31 @@ const EventsCard = ({
             <div className="min-w-[63px] eventDecoTwo rounded-full"></div>
             <div className="min-w-[63px] eventDecoThree rounded-tr-lg"></div>
           </div>
-          <div className="h-[168px] m-[18px] flex flex-col justify-between">
+          <div className="h-[198px] m-[18px] flex flex-col justify-between">
             <div>
               <h2 className="font-ibmBold">{EventTitle}</h2>
+              <p>{EventContent}</p>
             </div>
-            <div>{EventContent}</div>
-            <div
-              className={
-                EventDate >= moment().format("YYYY-MM-DD")
-                  ? EventCategory
-                  : "line-through"
-              }
-            >
-              {EventDate} - {EventTime}
-            </div>
-            <div>
-              <a
-                className="text-[18px] underline text-[#4562F8] font-ibmRegular"
-                href={EventLink}
-                target="_blank"
-                rel="noreferrer"
+            <div className="self-baseline">
+              <div
+                className={`self-baseline ${
+                  EventDate >= moment().format("YYYY-MM-DD")
+                    ? EventCategory
+                    : "line-through"
+                }`}
               >
-                {EventLinkText}
-              </a>
+                {EventDate} - {EventTime}
+              </div>
+              <div className="min-h-[2rem]">
+                <a
+                  className="text-[18px] underline text-[#4562F8] font-ibmRegular"
+                  href={EventLink}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {EventLinkText}
+                </a>
+              </div>
             </div>
           </div>
         </div>
